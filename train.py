e13511b854f2ee94bd8e97d90b12752268a140e6
seif khelifi <khelifiseif1@gmail.com>
2024-05-12 10:03:49 +0100

    created main program

--- train.py
+++ train.py
@@ -2,13 +2,16 @@ import torch
 import config
 from torch import nn
 from torch import optim
-from utils import gradient_penalty, load_checkpoint, save_checkpoint, plot_examples
+from utils import gradient_penalty
 from loss import VGGLoss
 from torch.utils.data import DataLoader
 from model import Generator, Discriminator, initialize_weights
 from tqdm import tqdm
-from dataset import MyImageFolder
-from torch.utils.tensorboard import SummaryWriter
+from dataset import SRDataset
+from config import *
+from model import *
+from loss import *
+
 
 torch.backends.cudnn.benchmark = True
 
@@ -97,4 +100,78 @@ def train(
                 'tb_step': tb_step,
                 'iteration': iteration
             }, checkpoint_path)
+            
 
+if __name__ == "__main__":
+    train_dataset = SRDataset(
+        root_dir=r'C:\Users\dell\Desktop\ESRGAN\data\DIV2K_train_HR',
+        lowres_transform=lowres_transform,
+        highres_transform=highres_transform,
+        both_transform=both_transforms
+    )
+
+    print(len(train_dataset))
+
+    val_dataset = SRDataset(
+        root_dir=r'C:\Users\dell\Desktop\ESRGAN\data\DIV2K_valid_HR',
+        lowres_transform=lowres_transform,
+        highres_transform=highres_transform,
+        both_transform=both_transforms
+    )
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        pin_memory=True,
+        num_workers=num_workers
+    )
+
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=1,
+        shuffle=True,
+        pin_memory=True,
+        num_workers=num_workers
+    )
+
+    generator = Generator(in_channels=num_channels, num_blocks=1).to(device)
+    discriminator = Discriminator(in_channels=num_channels).to(device)
+    init_weights(generator)
+    model = {
+        'discriminator': discriminator,
+        'generator': generator
+    }
+
+    optimizer_generator = torch.optim.Adam(model['generator'].parameters(), lr=lr, betas=(0.0, 0.9))
+    optimizer_discriminator = torch.optim.Adam(model['discriminator'].parameters(), lr=lr, betas=(0.0, 0.9))
+    optimizer = {
+        'discriminator': optimizer_discriminator,
+        'generator': optimizer_generator
+    }
+
+    criterion_generator = ESRGANLoss(l1_weight=1e-2, device=device)
+    criterion_discriminator = nn.BCELoss().to(device)
+    criterion = {
+        'discriminator': criterion_discriminator,
+        'generator': criterion_generator
+    }
+
+    generator_scaler = torch.cuda.amp.GradScaler()
+    discriminator_scaler = torch.cuda.amp.GradScaler()
+
+    tb_step = train(
+        loader=train_loader,
+        disc=model["discriminator"],
+        gen=model['generator'],
+        opt_gen=optimizer['generator'],
+        opt_disc=optimizer['discriminator'],
+        l1=nn.L1Loss(),
+        vgg_loss=VGGLoss(device),
+        g_scaler=generator_scaler,
+        d_scaler=discriminator_scaler,
+        tb_step=0,
+        device=device,
+        num_epochs=220,
+        checkpoint_path=fr"C:\Users\dell\Desktop\ESRGAN\checkpoint_epoch_185.pth"
+    )
